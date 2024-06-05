9a5a39aae6df2fbd83f3055aa52d197ed57c1527
ENES YILMAZ <x14916@gmail.com>
2024-06-05 16:05:15 +0300

    update

--- App.py
+++ App.py
@@ -1,75 +1,105 @@
 import streamlit as st
-from joblib import Parallel, delayed 
 import joblib
 import string
 import re
-import utils
+
+
+
+users = ["Netflix", "Webtekno", "Acun Ilıcalı", "Rasim Ozan Kutahyali"]
+
+# some meaningless words in dataset
+stop_words = sorted(
+    ['škoda','ɐpuıʞɐʎ','ɐɯnɔ','ɹıpɹɐʌ','ɹıq',
+     'ɹǝƃuɐɹʇs','ɹǝɥ','ˇωˇ','έλληνα','έχω','ήρθες',
+     'αδερφέ','αμφιβολία','αυτή','γνωρίζω','δεν','δυο',
+     'είμαι','είναι','εμάς','ενθουσιάζουν','ζήσουμε',
+     'θα','κάνουν','και','καλώς','καμία','κοινά','μαζί',
+     'μας','με','μεγάλα','μιας','μου','οι','οικογένειά',
+     'ομάδα','ομάδες','παίζουν','παιχνίδια','παράδειγμα',
+     'πιστεύεις','πολλά','πολύ','που','πράγματα','πρόθυμα',
+     'πόλη','σε','στην','τα','την','το','των','υπέροχα',
+     'φιλίας','χαρούμενο','χαρούμενος','χωρών','όμορφης',
+     'όνειρα','ότι','ⁿᵉᵈᵉⁿ','くへ','ノspoiler','ヽつ','내일',
+     '단독공개','오후','𝓐𝓜𝓟𝓘','𝓑𝓲𝔃','𝓨𝓞𝓝','𝓲𝔃','𝓸𝓵𝓪𝓬𝓪𝓰'
+    ])
+
 
 class App:
     def __init__(self):
         self.model_NB = joblib.load("./models/model_NB.pkl")
-        self.model_DT = joblib.load("./models/model_DT.pkl")
         self.vectorizer = joblib.load("./models/vectorizer.pkl")
         
     def clean(self, tweet):
         """
-        Bu fonksiyon tweetlerin icindeki anlamsiz kelimeleri, noktalama isaretlerini ve rakamlari cikararak daha sade bir bicime donusturur.
+        This function removes meaningless words, punctuation marks, numbers from tweets, transforming them into a simpler form.
 
         Parameters:
-        tweet (str): Temizlenecek olan tweet
+        tweet (str): The tweet to be cleaned
 
         Returns:
-        row (str): Temizlenmis tweet
+        row (str): Cleaned tweet
         """
-        satirlar = tweet.replace("\n", " ") # Veri setinde \n yerine " " (bosluk) koyuluyor.
-        satirlar = re.sub("[0-9]+", "", satirlar) # veri icerisindeki sayilar cikariliyor
-        satirlar = [t for t in satirlar.split()if t not in utils.stop_words] # veri icerisindeki anlamsiz kelimeler cikariliyor
+        satirlar = tweet.replace("\n", " ") # Replaces \n with a space in the dataset.
+        satirlar = re.sub("[0-9]+", "", satirlar) # Removes numbers from the data.
+        satirlar = [t for t in satirlar.split() if t not in stop_words] # Removes meaningless words from the data.
         
-        return " ".join(satirlar).lower().translate(str.maketrans("", "", string.punctuation)) # veri icindeki noktalama isaretleri temizleniyor.
+        return " ".join(satirlar).lower().translate(str.maketrans("", "", string.punctuation)) # Cleans punctuation marks from the data.
+
           
     def tweet_analyse(self, tweet):
         tweet = self.clean(tweet)
         tweet_vec = self.vectorizer.transform([tweet])
-        # Netflix: 0
-        # Webtekno: 1
-        # Acun: 2
-        # ROK: 3
-        print("NB:", self.model_NB.predict(tweet_vec))
-        print("DT:", self.model_DT.predict(tweet_vec))
         
-        result_NB = self.model_NB.predict_proba(tweet_vec)
-        result_DT = self.model_DT.predict_proba(tweet_vec)
-        # print(
-        #     f"Netflix: \tNB: {round(100*result_NB[0][0],2)} \tDT: {round(100*result_DT[0][0],2)}\n"
-        #     f"Webtekno: \tNB: {round(100*result_NB[0][1],2)} \tDT: {round(100*result_DT[0][1],2)}\n"
-        #     f"Acun Ilicali: \tNB: {round(100*result_NB[0][2],2)} \tDT: {round(100*result_DT[0][2],2)}\n"
-        #     f"Rasim Ozan Kutahyali: \tNB: {round(100*result_NB[0][3],2)} \tDT{round(100*result_DT[0][3],2)}"
-        #     )
+        result_NB = self.model_NB.predict_proba(tweet_vec)[0]
         
-        return [result_NB, result_DT]
+        return result_NB
     
+
+
+
     def run(self):
-        st.title("Tweet Analiz Sistemi")
-        st.markdown("---")
-        
-        form = st.form("tweet_analyse_form")
-        tweet = form.text_input("Analiz etmek istediğiniz Tweet'i giriniz:")
-        submitted = form.form_submit_button("Analiz Et")
-        
-        if submitted:
-            if tweet == "" or tweet == " ":
-                st.write("Herhangi bir tweet yazmadınız!")
-            else:
-                result_NB, result_DT = self.tweet_analyse(tweet)
-                
-                naive_bayes, decision_tree = st.columns(2)
-                
-                with naive_bayes:
-                    st.markdown("**Multi Nomial Naive Bayes Sonuçları**")
-                    for i, result in enumerate(result_NB[0]):
-                        st.text(f"{utils.users[i]}: \t{round(100*result,2)}")
-                
-                with decision_tree:
-                    st.markdown("**Decision Tree Sonuçları**")
-                    for i, result in enumerate(result_DT[0]):
-                        st.text(f"{utils.users[i]}: \t{round(100*result,2)}")
+       st.title("Tweet Analysis System")
+       
+       st.markdown(
+           """
+           <style>
+           .stApp{
+               background-color: #000;
+           }
+           .analyze-button {
+               background-color: #C6C6C6;
+               border: none;
+               color: black;
+               text-align: center;
+               text-decoration: none;
+               display: inline-block;
+               cursor: pointer;
+               border-radius: 2px;
+           }
+           </style>
+           """,
+           unsafe_allow_html=True
+       )
+       
+       form = st.form("tweet_analyse_form")
+       tweet = form.text_input("Type the tweet you want analyzed:")
+       submitted = form.form_submit_button("Analyse")
+       
+       if submitted:
+           if tweet == "" or tweet == " ":
+               st.write("You didn't write any tweets!")
+           else:
+               result_NB = self.tweet_analyse(tweet)
+               max_index = result_NB.argmax()
+   
+               st.markdown("<h4 style='text-align: center;'>Results of Multi-Nomial Naive Bayes</h4>", unsafe_allow_html=True)
+               
+               for i, score in enumerate(result_NB):
+                   if i == max_index:
+                       st.markdown(f"<button class='analyze-button'>{users[i]}</button>", unsafe_allow_html=True)
+                   else:
+                       st.markdown(f"{users[i]} \t")
+
+
+app = App()
+app.run()
